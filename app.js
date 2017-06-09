df85d70099b815f81c573a1593faf672a2dcb27f
sueanyv <valenciasue@hotmail.com>
2017-06-08 23:01:14 -0700

    adding to function

--- app.js
+++ app.js
@@ -1,20 +1,22 @@
+
 'use strict';
 
-
-
+var score = 0;
 var names = prompt('Hello! please enter your name.');
 alert(' Welcome to my page ' + names + '! Lets play a game, There are 7 question about me. lets see how many you can get correct. GOOD LUCK ');
-var score = 0;
-var live = prompt('Do I live in seattle? Yes or No').toUpperCase();
-if (live === 'YES' || live === 'Y') {
-  alert('Correct!');
-  score += 1;
-} else if (live === 'NO' || live === 'N') {
-  alert('Sorry you got it wrong!');
-} else {
-  alert('Wrong Input!');
-};
+var questions = ['Do I live in Seattle? Yes or No.', 'Is my favorite color Gold? Yes or No', 'Can I sing? Yes or No', 'Do I dislike pizza? Yes or No', 'Do I like to dance? Yes or No.', 'I am thinking of a random number between 1 & 6?.'];
+var answers = [['YES', 'Y'], ['YES', 'Y'], ['NO', 'N'], ['YES', 'Y'], ['YES', 'Y']];
 
+
+function questions1to5 (questionNumber) {
+  var answer = prompt(questions[questionNumber]).toUpperCase();
+  if (answer === answers[questionNumber][0] || answer === answers[questionNumber][1]) {
+    alert('Correct!');
+    score++;
+  } else {
+    alert('Sorry you got it wrong!');
+  }
+};
 console.log('the user \'s answer is ' + live + ' , ' + score);
 
 var gold = prompt('Is my favorite color Gold? Yes or No').toUpperCase();
@@ -113,7 +115,7 @@ if (score === 7) {
   alert('Congratulations!' + names + ' you got' + score + '/ 7 questions correct');
 
 } else {
-  alert('Sorry ' + names + 'you got ' + score + '/7 questions correct');
+  alert('Sorry ' + names + ' you got ' + score + '/7 questions correct');
 
 }
 
